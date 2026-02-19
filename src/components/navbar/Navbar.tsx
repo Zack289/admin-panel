74227d0ae6c80bf444e00b8918a49be37b3bcb8d
Zack289 <ozack911@gmail.com>
2026-02-19 16:00:08 +0545

    Implemented the navbar design

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,28 @@ import "./navbar.scss"
 const Navbar = () => {
   return (
     <div className='navbar'>
-      Navbar
+     <div className="logo">
+      <img src="/logo.svg" alt="logo" />
+      <span>Snow</span>
+     </div>
+
+     <div className="icons">
+      <img src="/search.svg" alt="" />
+      <img src="/app.svg" alt="" />
+      <img src="/expand.svg" alt="" />
+
+      <div className="notification">
+        <img src="/notifications.svg" alt="" />
+        <span>1</span>
+      </div>
+
+      <div className="user">
+        <img src="https://imgs.search.brave.com/dzfrT-ubH7nIwK12klU91N9AZgfQlK9crpFKYZ6z7xI/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDk2NTAz/NTIuanBn" alt="" />
+        <span>John</span>
+      </div>
+
+      <img src="/setting.svg" alt="" />
+     </div>
     </div>
   )
 }
